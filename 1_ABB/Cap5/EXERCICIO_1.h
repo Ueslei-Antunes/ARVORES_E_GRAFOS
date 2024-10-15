7127138d8fae6b00eaba2ec80d6788188b711ee5
Ueslei Antunes <antunesueslei5@gmail.com>
2024-10-15 07:34:32 -0300

    Update exercicio1

--- 1_ABB/Cap5/EXERCICIO_1.h
+++ 1_ABB/Cap5/EXERCICIO_1.h
@@ -3,7 +3,7 @@
 #include "ArvoreBinaria.h"
 #include "Utils.h"
 
-int iguais (pDArvore arvore1, pDArvore arvore2, FuncaoComparacao fc) {
+int iguais(pDArvore arvore1, pDArvore arvore2, FuncaoComparacao fc) {
     // Caso ambas as árvores sejam nulas, elas são iguais
     if (arvore1->raiz == NULL && arvore2->raiz == NULL) {
         return 1;
@@ -20,25 +20,7 @@ int iguais (pDArvore arvore1, pDArvore arvore2, FuncaoComparacao fc) {
     }
     
     // Comparar as subárvores esquerda e direita
-    pDArvore subArvoreEsquerda1 = (pDArvore) malloc(sizeof(DArvore));
-    pDArvore subArvoreEsquerda2 = (pDArvore) malloc(sizeof(DArvore));
-    pDArvore subArvoreDireita1 = (pDArvore) malloc(sizeof(DArvore));
-    pDArvore subArvoreDireita2 = (pDArvore) malloc(sizeof(DArvore));
-    
-    subArvoreEsquerda1->raiz = arvore1->raiz->esquerda;
-    subArvoreEsquerda2->raiz = arvore2->raiz->esquerda;
-    subArvoreDireita1->raiz = arvore1->raiz->direita;
-    subArvoreDireita2->raiz = arvore2->raiz->direita;
-    
-    int iguaisEsquerda = iguais(subArvoreEsquerda1, subArvoreEsquerda2, fc);
-    int iguaisDireita = iguais(subArvoreDireita1, subArvoreDireita2, fc);
-    
-    free(subArvoreEsquerda1);
-    free(subArvoreEsquerda2);
-    free(subArvoreDireita1);
-    free(subArvoreDireita2);
-    
-    // As árvores são iguais se ambas as subárvores forem iguais
-    return iguaisEsquerda && iguaisDireita;
+    return iguais(arvore1->raiz->esquerda, arvore2->raiz->esquerda, fc) &&
+           iguais(arvore1->raiz->direita, arvore2->raiz->direita, fc);
 }
 #endif
--- 1_ABB/Cap5/EXE_EXERCICIO.c
+++ 1_ABB/Cap5/EXE_EXERCICIO.c
@@ -5,36 +5,37 @@
 #include "Operacoes/0_struct_Descritor.h"
 #include "Operacoes/ArvoreBinaria/0_struct_Arvore.h"
 
-// Função iguais que compara duas árvores (conforme implementada anteriormente)
-int iguais(pDArvore arvore1, pDArvore arvore2, FuncaoComparacao fc);
 
+
+int iguais(pDArvore arvore1, pDArvore arvore2, FuncaoComparacao fc);
 int main() {
     // Criando duas árvores
     pDArvore arvore1 = criarArvore(0);
     pDArvore arvore2 = criarArvore(0);
-    
+
     // Incluindo nós nas duas árvores
     int valor1 = 10, valor2 = 5, valor3 = 20;
-    
+
     incluirInfo(arvore1, alocaInt(valor1), comparaInt);
     incluirInfo(arvore1, alocaInt(valor2), comparaInt);
     incluirInfo(arvore1, alocaInt(valor3), comparaInt);
-    
+
     incluirInfo(arvore2, alocaInt(valor1), comparaInt);
     incluirInfo(arvore2, alocaInt(valor2), comparaInt);
     incluirInfo(arvore2, alocaInt(valor3), comparaInt);
-    
+
     // Verificando se as árvores são iguais
     if (iguais(arvore1, arvore2, comparaInt)) {
         printf("As árvores são iguais!\n");
     } else {
         printf("As árvores são diferentes!\n");
     }
-    
+
     // Alterando uma árvore para fazer o teste com árvores diferentes
     int valorDiferente = 15;
     incluirInfo(arvore2, alocaInt(valorDiferente), comparaInt);
-    
+
+    // Verificando se as árvores são iguais novamente
     if (iguais(arvore1, arvore2, comparaInt)) {
         printf("As árvores são iguais!\n");
     } else {
